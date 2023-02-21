ad19f863ba95e80f36d0c5bcee9a119ba813c843
alpsar4l <metehansaral@gmail.com>
2023-02-21 16:19:23 +0300

    fixed

--- jetson.py
+++ jetson.py
@@ -36,11 +36,12 @@ while True:
         # frame = cv2.resize(frame, (640, 350), interpolation=cv2.INTER_AREA)
         frame = cv2.flip(frame, 1, 0)
 
+        """
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+           cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        """
 
         result, frame = cv2.imencode(".jpg", frame, [int(cv2.IMWRITE_JPEG_QUALITY), 20])
         data = base64.b64encode(frame)
